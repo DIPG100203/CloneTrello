0fd1099c1ca4094201ecddbec17c43c47841b93f
Daniel Ivan Guerrero Ponce <danielivanguerreroponce@gamil.com>
2026-02-02 13:56:25 -0600

    final de table y de filtro

---
+++ src/app/pages/table/data-source.ts
@@ -0,0 +1,47 @@
+import { DataSource } from "@angular/cdk/collections"
+import { Product } from "../../models/product"
+import { BehaviorSubject, Observable } from "rxjs"
+
+
+export class DataSourceProduct extends DataSource<Product> {
+
+    data = new BehaviorSubject<Product[]>([])
+    originalData: Product[] = []
+
+    connect(): Observable<Product[]> {
+        return this.data;
+    }
+
+    init(products: Product[]) {
+        this.originalData = products;
+        this.data.next(products)
+    }
+
+    getTotalAll() {
+        const products = this.data.getValue();
+        return products.map(item => item.price)
+        .reduce((price, total) => price + total, 0)
+    }
+
+    update(id: Product['id'], changes: Partial<Product>) {
+        const products = this.data.getValue()
+        const productIndex = products.findIndex(items => items.id === id)
+        if (productIndex !== -1) {
+            products[productIndex] = {
+                ...products[productIndex],
+                ...changes,
+            }
+            this.data.next(products)
+        }
+    }
+
+    find(query: string) {
+        const newProducts = this.originalData.filter(item => item.title.toLowerCase().includes(query.toLowerCase()))
+
+        this.data.next(newProducts)
+
+    }
+
+    disconnect() {}
+
+}
--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Product } from '../../models/product';
+import {CdkTableModule} from '@angular/cdk/table';
+import { CommonModule } from '@angular/common';
+import { DataSourceProduct } from './data-source';
+import { BtnComponent } from "../../components/btn/btn.component";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime } from 'rxjs';
+
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CdkTableModule, HttpClientModule, CommonModule, BtnComponent, ReactiveFormsModule],
   templateUrl: './table.component.html',
   styles: ``
 })
-export class TableComponent {
+export class TableComponent implements OnInit {
+
+  dataSource = new DataSourceProduct()
+  columns: string[] = ['id', 'title', 'price', 'images', 'Actions'];
+  total = 0; 
+  input = new FormControl('', {nonNullable: true})
+
+
+  constructor(private http: HttpClient) {}
+
+
+  ngOnInit(): void{
+    
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+    .subscribe(data => {
+      this.dataSource.init(data);
+      this.total = this.dataSource.getTotalAll();
+    })
+
+    this.input.valueChanges
+    .pipe(debounceTime(300))
+    .subscribe(value => {
+      this.dataSource.find(value)
+    })
+  }
+
+  update(product: Product) {
+
+    this.dataSource.update(product.id, {price: 20})
+  }
 
 }
